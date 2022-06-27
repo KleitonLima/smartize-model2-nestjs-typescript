dbdb748800bb6de3ebc588edf5d352d018d47df1
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-27 18:12:25 -0300

    fix: Alterado controllers e providers do users.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from 'src/app.controller';
-import { AppService } from 'src/app.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
