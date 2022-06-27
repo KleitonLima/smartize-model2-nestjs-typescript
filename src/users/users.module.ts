f47bcca0746f1d9224fb309148770329bd7769fb
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-27 15:41:02 -0300

    feat: users.module

---
+++ src/users/users.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class UsersModule {}
