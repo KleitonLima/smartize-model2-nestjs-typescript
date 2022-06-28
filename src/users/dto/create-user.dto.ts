b9a2ea89dad0eeb8133ec40e15b5cd7b73d6d776
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-27 22:22:38 -0300

    feat(users.dto) Aplicado o class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsAlphanumeric, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsAlphanumeric()
   password: string;
 }
