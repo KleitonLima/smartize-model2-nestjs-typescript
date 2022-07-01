b97c1aacfe71db5c48d9f711a4d4f52693748ee8
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-01 17:21:24 -0300

    feat(games): CRUD dos games criado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, GamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
