08f36266a0752340fe621f4c10106191fe0dfcde
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-05 22:22:18 -0300

    feat: criação e configuração inicial do módulo carts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
-  imports: [UsersModule, GamesModule],
+  imports: [UsersModule, GamesModule, CartsModule],
   controllers: [AppController],
   providers: [AppService],
 })
