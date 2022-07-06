84e9aacdfffb81841f1e3869de26b7173ad837eb
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-05 21:14:30 -0300

    fix: .at aplicado ao splitedMessage

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -25,9 +25,9 @@ export class UsersService {
   handleErrorConstraintUnique(error: Error): never {
     const splitedMessage = error.message.split('`');
 
-    const errorMessage = `O campo '${
-      splitedMessage[splitedMessage.length - 2]
-    }' não está respeitando a constraint UNIQUE`;
+    const errorMessage = `O campo '${splitedMessage.at(
+      -2,
+    )}' não está respeitando a constraint UNIQUE`;
 
     throw new UnprocessableEntityException(errorMessage);
   }
