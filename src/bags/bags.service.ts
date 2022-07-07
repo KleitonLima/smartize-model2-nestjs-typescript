3f5a543093be3b9b394970fe21754fd424928764
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-06 21:10:58 -0300

    feat: incluída funções de validação do id e do unique

--- src/bags/bags.service.ts
+++ src/bags/bags.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBagDto } from './dto/create-bag.dto';
 import { UpdateBagDto } from './dto/update-bag.dto';
+import { Bag } from './entities/bag.entity';
 
 @Injectable()
 export class BagsService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async verifyIdAndReturnBag(id: string): Promise<Bag> {
+    const bag: Bag = await this.prisma.bag.findUnique({ where: { id } });
+
+    if (!bag) {
+      throw new NotFoundException(`O id ${id} não é válido`);
+    }
+    return bag;
+  }
 
   create(dto: CreateBagDto) {
     return 'This action adds a new bag';
