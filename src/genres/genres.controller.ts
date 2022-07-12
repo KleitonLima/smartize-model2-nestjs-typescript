6078eec45c41f82a1960d8ed407127d5b275a2de
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-11 23:58:45 -0300

    feat: adicionada documentação as rotas

--- src/genres/genres.controller.ts
+++ src/genres/genres.controller.ts
@@ -10,32 +10,48 @@ import {
 import { GenresService } from './genres.service';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('genres')
 export class GenresController {
   constructor(private readonly genresService: GenresService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar novo gênero',
+  })
   create(@Body() dto: CreateGenreDto) {
     return this.genresService.create(dto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Exibir todos os gêneros',
+  })
   findAll() {
     return this.genresService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Exibir um gênero',
+  })
   findOne(@Param('id') id: string) {
     return this.genresService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um gênero',
+  })
   update(@Param('id') id: string, @Body() dto: UpdateGenreDto) {
     return this.genresService.update(id, dto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar um jogo',
+  })
   remove(@Param('id') id: string) {
     return this.genresService.remove(id);
   }
