cffa1d3da18b53f8740b906f2f490a08cc826cf8
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-12 10:53:41 -0300

    feat: configurado dto

--- src/genres/dto/create-genre.dto.ts
+++ src/genres/dto/create-genre.dto.ts
@@ -1,1 +1,12 @@
-export class CreateGenreDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateGenreDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Nome da categoria',
+    example: 'Ação e aventura',
+  })
+  name: string;
+}
