cb88c519e16a7eb751c2f2602cb217379bcd2fb7
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-11 23:44:19 -0300

    feat: novo módulo "genres" criado já com CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
 import { BagsModule } from './bags/bags.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [UsersModule, GamesModule, BagsModule],
+  imports: [UsersModule, GamesModule, BagsModule, GenresModule],
   controllers: [AppController],
   providers: [AppService],
 })
