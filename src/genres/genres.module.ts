75234b7498274115a86ec38f8ac2d744c336c9c6
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-11 23:45:34 -0300

    feat: importado módulo prisma

--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GenresService } from './genres.service';
 import { GenresController } from './genres.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GenresController],
-  providers: [GenresService]
+  providers: [GenresService],
 })
 export class GenresModule {}
