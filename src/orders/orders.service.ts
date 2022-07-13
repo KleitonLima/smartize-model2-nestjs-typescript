98c7e01529adcfda04c6f0d373cce96bfdfcdfad
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-13 13:31:08 -0300

    feat: configurado o create

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
@@ -8,7 +9,36 @@ export class OrdersService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(dto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const data: Prisma.OrderCreateInput = {
+      bag: {
+        connect: {
+          number: dto.bagNumber,
+        },
+      },
+      user: {
+        connect: {
+          id: dto.userId,
+        },
+      },
+      games: {
+        connect: dto.games.map((element) => ({ id: element })),
+      },
+    };
+
+    return this.prisma.order.create({
+      data,
+      select: {
+        id: true,
+        createdAt: true,
+        bagNumber: true,
+        userId: true,
+        games: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 
   findAll() {
