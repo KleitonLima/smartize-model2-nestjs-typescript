fb474fcd3da50be062aaa86390142288513ada92
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-13 21:36:36 -0300

    fix: removido orderId

--- src/orders/dto/create-order-to-game.dto.ts
+++ src/orders/dto/create-order-to-game.dto.ts
@@ -4,13 +4,6 @@ import { IsUUID } from 'class-validator';
 export class CreateOrderToGameDto {
   @IsUUID()
   @ApiProperty({
-    description: 'Id do pedido',
-    example: 'a966f158-6dec-454c-9c72-9af6c90a8062',
-  })
-  orderId: string;
-
-  @IsUUID()
-  @ApiProperty({
     description: 'Id do jogo',
     example: 'a966f158-6dec-454c-9c72-9af6c90a8062',
   })
