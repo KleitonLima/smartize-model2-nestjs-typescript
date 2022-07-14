f10abbcd44845f8c969eb668a28116b9eff42996
Kleiton Lima <kleiton.mini@gmail.com>
2022-07-13 21:24:45 -0300

    fix: class renomeada

--- src/orders/dto/create-order-to-game.dto.ts
+++ src/orders/dto/create-order-to-game.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 
-export class CreateOrderToProductDto {
+export class CreateOrderToGameDto {
   @IsUUID()
   @ApiProperty({
     description: 'Id do pedido',
