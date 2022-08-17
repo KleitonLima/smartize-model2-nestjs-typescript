2524427533c84e17d3e2ea644e25a16e611560f2
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-17 19:35:27 -0300

    fix: link do deploy adicionado

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3333/docs for Swagger docs...';
+    return 'Server is running! 🚀\n Please check https://smartize-store-back-m4-production.up.railway.app/docs for Swagger docs...';
   }
 }
